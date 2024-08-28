a3f2b6a454d7e6549fadcdb08f433a87d5dafcfc
Maurindo Junior <maurindoapgjunior@gmail.com>
2024-08-28 15:57:22 -0300

    colocando conteudo das rotas biografia

---
+++ src/components/biografia/index.jsx
@@ -0,0 +1,43 @@
+import styles from './biografia.module.css'
+
+export default function BiografiaBlog(){
+    return(
+        <div className={styles.corpo}>
+            <h1>Biografia</h1>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum egestas lobortis tellus eu commodo. 
+                Donec at dignissim justo. Nam viverra odio vitae placerat iaculis. 
+                In ac lorem eget velit facilisis rutrum in eu nisi. Duis commodo odio sed nisi blandit, vel feugiat mi egestas. 
+                Fusce et leo eu leo tristique volutpat eget eu diam. Sed lobortis imperdiet sapien, ac malesuada quam facilisis vel. 
+                Nunc blandit maximus ex, eget ullamcorper augue blandit ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                Phasellus dignissim lorem vel ex finibus pharetra. Sed finibus tincidunt elit sed placerat. Curabitur vitae mattis quam, 
+                a auctor purus. Nulla non pharetra felis. Donec commodo risus a lacinia dictum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum egestas lobortis tellus eu commodo. 
+                Donec at dignissim justo. Nam viverra odio vitae placerat iaculis. 
+                In ac lorem eget velit facilisis rutrum in eu nisi. Duis commodo odio sed nisi blandit, vel feugiat mi egestas. 
+                Fusce et leo eu leo tristique volutpat eget eu diam. Sed lobortis imperdiet sapien, ac malesuada quam facilisis vel. 
+                Nunc blandit maximus ex, eget ullamcorper augue blandit ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                Phasellus dignissim lorem vel ex finibus pharetra. Sed finibus tincidunt elit sed placerat. Curabitur vitae mattis quam, 
+                a auctor purus. Nulla non pharetra felis. Donec commodo risus a lacinia dictum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum egestas lobortis tellus eu commodo. 
+                Donec at dignissim justo. Nam viverra odio vitae placerat iaculis. 
+                In ac lorem eget velit facilisis rutrum in eu nisi. Duis commodo odio sed nisi blandit, vel feugiat mi egestas. 
+                Fusce et leo eu leo tristique volutpat eget eu diam. Sed lobortis imperdiet sapien, ac malesuada quam facilisis vel. 
+                Nunc blandit maximus ex, eget ullamcorper augue blandit ac. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                Phasellus dignissim lorem vel ex finibus pharetra. Sed finibus tincidunt elit sed placerat. Curabitur vitae mattis quam, 
+                a auctor purus. Nulla non pharetra felis. Donec commodo risus a lacinia dictum.
+            </p>
+
+
+            <div className={styles.final}>
+                Todos os direitos reservados
+            </div>
+        </div>
+    );
+}
--- src/paginas/biografia.jsx
+++ src/paginas/biografia.jsx
@@ -1,10 +1,12 @@
 import Capa from "../components/capa";
 import Menu from "../components/menu";
+import BiografiaBlog from "../components/biografia";
 function Biografia(){
     return(
       <div>
           <Capa />
           <Menu />
+          <BiografiaBlog />
       </div>
     );
 }
